3ae54d3447ff46705642f4709d29f5cc70d7f7ba
Arun Nair <arun@Aruns-MacBook-Air.local>
2025-06-18 22:31:52 +0530

    Added pre-commit hooks, updated requirements.txt

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/home")
 def home_page():
     return "<h1>Home Page</h1>"
+
 
 @app.route("/about")
 def about_page():
