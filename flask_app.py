1cfac4238dabcf7cd81a01837f1f2823fac9c61e
Arun Nair <31363139+arun-nair-sde@users.noreply.github.com>
2025-06-20 13:32:16 +0530

    Updated readme, Added dummy posts

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,34 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+posts = [
+    {
+        "author": "Arun",
+        "title": "First Post",
+        "content": "First post content.",
+        "created_at": "June 19, 2025"
+    },
+    {
+        "author": "Anonymous",
+        "title": "Second Post",
+        "content": "Second post content.",
+        "created_at": "June 20, 2025"
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about_page():
-    return "About Us"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
